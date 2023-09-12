80fc4b2486b4a8496f18ae59f759722fa4b4dae5
dagwan <dagwanpan@gmail.com>
2023-09-12 15:16:33 +0100

    adding name of someone i know to the root pather (Home Page)

--- controllers/lesson1.js
+++ controllers/lesson1.js
@@ -1,16 +1,26 @@
-const dagwanRoute = (req, res) => {
+// Import necessary modules or dependencies if required.
+
+// Define a controller function for the "dagwanRoute" route.
+const nanretRoute = (req, res) => {
+  // Send a response to the client with the text "Nanret Pan'an".
   res.send("Nanret Pan'an");
 }
+
+// Define a controller function for the "emmanuelRoute" route.
 const emmanuelRoute = (req, res) => {
-    res.send("Emmanuel Daniel");
-  }
+  // Send a response to the client with the text "Emmanuel Daniel".
+  res.send("Emmanuel Daniel");
+}
 
-  const adamsRoute = (req, res) => {
-    res.send("Adams Charles");
-  }
+// Define a controller function for the "adamsRoute" route.
+const adamsRoute = (req, res) => {
+  // Send a response to the client with the text "Adams Charles".
+  res.send("Adams Charles");
+}
 
-  module.exports = {
-    emmanuelRoute,
-    dagwanRoute,
-    adamsRoute,
-  };
+// Export the controller functions to make them available to other parts of the application.
+module.exports = {
+  nanretRoute,
+  emmanuelRoute,
+  adamsRoute,
+};
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
+// Import the Express framework and create an Express application instance.
 const express = require('express');
 const app = express();
 
-const port = 3000
+// Define the port number on which the server will listen.
+const port = 3000;
 
-app.use('/', require('./routes/index'))
- 
+// Use the routes defined in the 'index.js' file for all incoming requests at the root path ('/').
+app.use('/', require('./routes/index'));
+
+// Start the Express server and listen on the specified port or use the default port (3000).
 app.listen(process.env.PORT || port, () => {
+  // Log a message to indicate that the web server is listening on the chosen port.
   console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
 });
