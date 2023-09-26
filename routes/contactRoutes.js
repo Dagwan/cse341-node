5d7a6e8427ae7508c3f19c4abafe01e5eca2808f
dagwan <dagwanpan@gmail.com>
2023-09-26 12:51:27 +0100

    deployment

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -3,18 +3,18 @@ const router = express.Router();
 const contactController = require('../controllers/contactController');
 
 // Create a new contact
-router.post('/contacts', contactController.createContact);
+router.post('/', contactController.createContact);
 
 // Read all contacts
-router.get('/contacts/', contactController.getAllContacts);
+router.get('/', contactController.getAllContacts);
 
 // Read a contact by ID
-router.get('/contacts/:id', contactController.getContactById);
+router.get('/:id', contactController.getContactById);
 
 // Update a contact by ID
-router.put('/contacts/:id', contactController.updateContactById);
+router.put('/:id', contactController.updateContactById);
 
 // Delete a contact by ID
-router.delete('/contact/:id', contactController.deleteContactById);
+router.delete('/:id', contactController.deleteContactById);
 
 module.exports = router;
